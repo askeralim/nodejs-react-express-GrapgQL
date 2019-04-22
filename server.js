14d02b639224cd126e3220fd790f929507626693
Askerali Maruthullathil <askeralim@gmail.com>
2019-04-22 16:02:26 +0300

    Client Part Added

--- server.js
+++ server.js
@@ -2,11 +2,28 @@ const express = require("express");
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema')
 
+const cors = require('cors');
+const path = require('path');
+
 const app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: true
-}));
+
+// Allow cross-origin
+app.use(cors());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,()=> console.log(`Server Started at: ${PORT}`));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
